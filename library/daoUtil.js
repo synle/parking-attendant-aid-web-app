0fe6976e8f72d941e2460f78bb70c721e255efbd
Sy Le <lenguyensy@gmail.com>
2017-11-19 17:09:19 -0800

    core dao and low level utils

---
+++ library/daoUtil.js
@@ -0,0 +1,96 @@
+const Sequelize = require('sequelize');
+const Table = require('sequelize-simple-adapter');
+
+const shouldUseCloud = process.env.DB_HOST
+    && process.env.DB_NAME
+    && process.env.DB_USER
+    && process.env.DB_PASSWORD;
+
+
+// if there is a db connection, then use it...
+const sequelize = !!process.env.MAIN_DB_HOST
+    ? new Sequelize(
+        process.env.MAIN_DB_NAME,
+        process.env.MAIN_DB_USER,
+        process.env.MAIN_DB_PASSWORD,
+        {
+            host: process.env.MAIN_DB_HOST,
+            dialect: 'mssql',
+            logging: false,
+            pool: {
+                max: 5,
+                min: 0,
+            },
+            dialectOptions: {
+                encrypt: true
+            },
+        }
+    )
+    : new Sequelize(
+        'db_user', // 'database',
+        '', // 'username',
+        '', // 'password',
+        {
+            dialect: 'sqlite',
+            storage: './db.sqlite3',
+            logging: false
+        }
+    );
+
+// might only need to run for init call...
+const promiseSequelizeInit = sequelize.sync().then(function (argument) {
+    // TODO: remove this log line...
+    console.log('Database...synced... read to use', process.env.MAIN_DB_HOST);
+});
+
+
+const User = sequelize.define('parking_users', {
+    id               : { type: Sequelize.DataTypes.UUID, defaultValue: Sequelize.DataTypes.UUIDV1, primaryKey: true },
+    email            : { type: Sequelize.STRING },
+    password         : { type: Sequelize.STRING },
+    firstName        : { type: Sequelize.STRING },
+    lastName         : { type: Sequelize.STRING },
+});
+
+
+const AccessToken = sequelize.define('parking_accesses', {
+    id               : { type: Sequelize.DataTypes.UUID, defaultValue: Sequelize.DataTypes.UUIDV1, primaryKey: true },
+    userId           : { type: Sequelize.DataTypes.UUID },
+    token            : { type: Sequelize.STRING },
+    expired          : { type: Sequelize.BOOLEAN, defaultValue: false },
+});
+
+
+const Violation = sequelize.define('parking_violations', {
+    id               : { type: Sequelize.DataTypes.UUID, defaultValue: Sequelize.DataTypes.UUIDV1, primaryKey: true },
+    userId           : { type: Sequelize.DataTypes.UUID },
+    licenseNumber    : { type: Sequelize.STRING },
+    description      : { type: Sequelize.STRING },
+    violationTime    : { type: Sequelize.DATE },
+    long             : { type: Sequelize.FLOAT },
+    lat              : { type: Sequelize.FLOAT },
+    fineAmount       : { type: Sequelize.FLOAT },
+    paid             : { type: Sequelize.BOOLEAN, defaultValue: false },
+});
+
+
+const Note = sequelize.define('parking_notes', {
+    id               : { type: Sequelize.DataTypes.UUID, defaultValue: Sequelize.DataTypes.UUIDV1, primaryKey: true },
+    userId           : { type: Sequelize.DataTypes.UUID },
+    licenseNumber    : { type: Sequelize.STRING },
+    description      : { type: Sequelize.STRING },
+    recordTime       : { type: Sequelize.DATE },
+    long             : { type: Sequelize.FLOAT },
+    lat              : { type: Sequelize.FLOAT },
+});
+
+
+module.exports = {
+    init: async () => {
+        await promiseSequelizeInit;
+    },
+    User        : new Table(User, promiseSequelizeInit),
+    AccessToken : new Table(AccessToken, promiseSequelizeInit),
+    Note        : new Table(Note, promiseSequelizeInit),
+    Violation   : new Table(Violation, promiseSequelizeInit),
+}
---
+++ library/resourceUtil.js
@@ -0,0 +1,210 @@
+// external
+var uuid = require('node-uuid');
+
+// internal
+var daoUtil = require('./daoUtil');
+
+
+var resourceUtil = {
+  createUserLocal: function(email, firstName, lastName, password){
+    var encryptedPassword = _getEncryptedPassword(password);
+    return daoUtil.User.create({
+      email: email,
+      firstName: firstName,
+      lastName: lastName,
+      password: encryptedPassword,
+    });
+  },
+  findLocalUserByEmailAndPassword: function(email, password){
+    var encryptedPassword = _getEncryptedPassword(password);
+    return daoUtil.User.findAll({
+      attributes: { exclude: ['password'] },
+      where: { email: email, password: encryptedPassword, }
+    });
+  },
+  findLocalUser: function(email){
+    return daoUtil.User.findAll({
+      attributes: { exclude: ['password'] },
+      where: { email: email }
+    });
+  },
+  findLocalUserByUserId: function(userId){
+    return daoUtil.User.findOne({
+      attributes: { exclude: ['password'] },
+      where: { id: userId }
+    });
+  },
+
+  findLocalUserByAccessToken: function(accessToken){
+    // check access token is valid, then get user info...
+    return daoUtil.AccessToken.findOne({
+        where: { token: accessToken, expired: false }
+      }).then(function(accessTokenObject){
+        return resourceUtil.findLocalUserByUserId(accessTokenObject.userId);
+      })
+  },
+
+
+  // access token
+  loginWithMobile: function(email, password){
+    var encryptedPassword = _getEncryptedPassword(password);
+    var promiseFoundUser = new Promise(function(resolve, reject){
+      daoUtil.User.findOne({
+        attributes: { exclude: ['password'] },
+        where: { email: email, password: encryptedPassword, }
+      }).then(
+        function(foundUser){
+          // found user, then create token...
+          var tokenObject = {
+            userId  : foundUser.id,
+            token   : _generateRandomAccessToken(),
+            expired : false
+          };
+
+          return daoUtil.AccessToken.create(tokenObject).then(
+            resolve.bind(
+              null,
+              Object.assign(
+                {token: tokenObject.token},
+                foundUser
+              )
+            ),
+            reject
+          )
+        },
+        function(error){
+          reject();
+        }
+      );
+    });
+
+
+    return promiseFoundUser;
+  },
+
+
+  logoutWithMobile: function(authToken){
+    return daoUtil.AccessToken.destroy({
+      where: { token: authToken }
+    });
+  },
+
+
+  // notes
+  createNote: function({
+    userId,
+    licenseNumber,
+    description,
+    recordTime,
+    long,
+    lat
+  }){
+    return daoUtil.Note.create({
+      userId,
+      licenseNumber,
+      description,
+      recordTime,
+      long,
+      lat
+    })
+  },
+
+
+  getAllNotes: function(){
+    return daoUtil.Note.findAll({
+      where: {},
+      order: '"updatedAt" DESC'
+    });
+  },
+
+
+
+  getAllNotesByLicenseNumber: function(licenseNumber){
+    return daoUtil.Note.findAll({
+      where: {licenseNumber},
+      order: '"updatedAt" DESC'
+    });
+  },
+
+
+  getAllNotestionsByUserId: function(userId){
+    return daoUtil.Note.findAll({
+      where: {userId},
+      order: '"updatedAt" DESC'
+    });
+  },
+
+
+
+
+
+  // violations
+  createViolation: function({
+    userId,
+    licenseNumber,
+    description,
+    violationTime,
+    long,
+    lat,
+    fineAmount,
+    paid
+  }){
+    return daoUtil.Violation.create({
+      userId,
+      licenseNumber,
+      description,
+      violationTime,
+      long,
+      lat,
+      fineAmount,
+      paid
+    })
+  },
+
+
+
+  getAllViolations: function(){
+    return daoUtil.Violation.findAll({
+      where: {},
+      order: '"updatedAt" DESC'
+    });
+  },
+
+
+  getAllViolationsByLicenseNumber: function(licenseNumber){
+    return daoUtil.Violation.findAll({
+      where: {licenseNumber},
+      order: '"updatedAt" DESC'
+    });
+  },
+
+
+  getAllViolationsByUserId: function(userId){
+    return daoUtil.Violation.findAll({
+      where: {userId},
+      order: '"updatedAt" DESC'
+    });
+  },
+
+
+
+
+};
+
+function _getEncryptedPassword(password){
+  return password;
+}
+
+function _generateRandomAccessToken() {
+  // https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 256; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return uuid.v4() + '-' + text;
+}
+
+
+module.exports = resourceUtil;
