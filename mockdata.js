be2fc1e1e9989229a4d2a8744ed469c55384eb86
Sy Le <lenguyensy@gmail.com>
2017-12-01 08:28:45 -0800

    getting the mock user part up...

--- mockdata.js
+++ mockdata.js
@@ -4,6 +4,35 @@ const daoUtil = require('./library/daoUtil');
 async function _doWork(){
     await daoUtil.init();
     console.log('Start Mocking');
+    const Mock_Users = [{
+            email: 'syle@syle.com',
+            password: 'password',
+            firstName: 'Sy',
+            lastName: 'Le',
+        }],
+        Mock_Violations = [],
+        Mock_Notes = [];
+
+    let NUM_MOCK_USER = 5;
+    while(NUM_MOCK_USER > 0){
+        NUM_MOCK_USER--;
+
+        Mock_Users.push({
+            email: dataMockUtil.getEmail(),
+            password: 'password',
+            firstName: dataMockUtil.getNameWord(),
+            lastName: dataMockUtil.getNameWord(),
+        });
+    }
+
+
+    console.log(Mock_Users);
+
+
+
+
+
+    console.log('Insert to DB');
 }
 
 
